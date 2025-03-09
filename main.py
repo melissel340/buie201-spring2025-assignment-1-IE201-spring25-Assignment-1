bca9f93261f0d3c9270ac2d97f63cdbefa373267
Melis Sel <melissel.com>
2025-03-09 12:16:37 +0300

    version1

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 def int_to_roman(num):
-    """
-    Convert an integer to a Roman numeral.
-
-    :param num: Integer value between 1 and 3999 inclusive.
-    :return: A string representing the Roman numeral of the integer.
-    """
+    dec_1_int = num // 1000
+    num = num - dec_1_int*1000
+    print(f"{num}, {dec_1_int}")
+    dec_2_int = (num // 100)
+    num = num - dec_2_int*100
+    print(f"{num}, {dec_2_int}")
+    dec_3_int = num // 10
+    num = num - dec_3_int*10
+    print(f"{num}, {dec_3_int}")
+    dec_4_int = num
+    print(f"{num}, {dec_4_int}")
+int_to_roman(3333)
